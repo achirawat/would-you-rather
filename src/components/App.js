b02791a30b629785d7c21b389deb1193daf8e336
Achirawat Juasawad <achirawat@angstromsolutions.net>
2018-09-28 17:13:08 +0700

    add file in component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav'
 
 class App extends Component {
   render() {
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='new' exact activeClassName='active'>New Question</NavLink>
+          </li>
+          <li>
+            <NavLink to='leader' exact activeClassName='active'>Leader Board</NavLink>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
